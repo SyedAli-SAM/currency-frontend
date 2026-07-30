ea9a54f043e82cb2b6051bf0c89ff67a37ad239e
SyedAli-SAM <khan1-9@hotmail.co.uk>
2026-07-31 02:56:33 +0500

    environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://currency-backend-36r4.onrender.com/currency',
+    apiUrl: 'https://currency-backend-1-2zmn.onrender.com/currency',
   };
